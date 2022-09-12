d2b52fd1eca7f49a6294e0aaaf19a77e3f2a8c54
Daniel <danielderu3@gmail.com>
2022-09-12 12:44:18 +0200

    added separate lock.php file for the lock file function

--- cron.php
+++ cron.php
@@ -2,16 +2,9 @@
 require(dirname(__FILE__) . '/../../../wp-config.php');
 require_once "rectron.php";
 require_once(dirname(__FILE__) . '/includes/print.php');
+require_once(dirname(__FILE__) . '/includes/lock.php');
 
-function run_cron(){
-
-    $cron_file = fopen("./cron.text", "w");
-    fwrite($cron_file, "The cron job fired at: " . date("F j, Y, g:i a"));
-    fclose($cron_file);
-
-    $lock_file = fopen("lock.txt", "w+");
-    if(!flock($lock_file,  LOCK_EX | LOCK_NB)) return;
-
+$import_feed = function (){
     try{
         $time_start = microtime(true);
         $mem_start = memory_get_usage(true);
@@ -27,9 +20,18 @@ function run_cron(){
     } catch (Exception $e){
         format($e);
     }
+};
 
-    flock($lock_file, LOCK_UN);
-    fclose($lock_file);
+function run_cron(){
+    global $import_feed;
+    
+    $cron_file = fopen(dirname(__FILE__) . "/cron.txt", "w");
+    fwrite($cron_file, "The cron job fired at: " . date("F j, Y, g:i a"));
+    fclose($cron_file);
+
+    run_with_lock($import_feed);
 }
+
+
 
 if(is_plugin_active("lk_supplier/smart_feeds.php" )) run_cron();
---
+++ includes/lock.php
@@ -0,0 +1,18 @@
+<?php
+require_once(dirname(__FILE__) . '/print.php');
+
+
+function run_with_lock($run_task){
+    $lock_file = dirname(__FILE__) . "/../lock.txt";
+    $lock_file_stream = fopen($lock_file, "w+");
+
+    if(!flock($lock_file_stream,  LOCK_EX | LOCK_NB)){
+        format("Cron Job skipped because the previous job didn;t finish yet");
+        return;
+    }
+
+    $run_task();
+
+    flock($lock_file_stream, LOCK_UN);
+    fclose($lock_file_stream);
+}
